97c0607cf62cd9366bd53103e6eadb218a845659
fapjsc <p82szh@gmail.com>
2021-02-14 07:12:13 +0800

    before backend server

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,5 +1,101 @@
 <template>
-  <div>
-    <h1>Contact</h1>
-  </div>
+  <form @submit.prevent="formSubmit">
+    <div class="form-control">
+      <label for="email">Your email</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+
+    <div class="form-control">
+      <label for="message">message</label>
+      <textarea id="message" rows="5" v-model.trim="message"></textarea>
+    </div>
+
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+
+    <p v-if="!formIsValid" class="errors">
+      請輸入正確的emal或是message不得爲空
+    </p>
+  </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true,
+    };
+  },
+
+  methods: {
+    formSubmit() {
+      this.formIsValid = true;
+
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.message === ''
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id,
+      });
+
+      this.$router.replace('/coaches');
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import coachModule from './modules/coaches/index';
+import requestsModule from './modules/requests/index';
 
 const store = createStore({
   modules: {
-    coaches: coachModule
+    coaches: coachModule,
+    requests: requestsModule
   },
   state() {
     return {
